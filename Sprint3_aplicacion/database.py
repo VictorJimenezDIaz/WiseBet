38af10cee34df1cf507062c84cedcb0bf7fd09c0
javicuarteroUCLM <javier.cuartero@alu.uclm.es>
2024-04-11 16:43:42 +0200

    Revert "Actualización de cambios"
    This reverts commit 30f1fa7661d4a76f7c37b83eddd3f6939d83659a.

--- Sprint3_aplicacion/database.py
+++ Sprint3_aplicacion/database.py
@@ -8,4 +8,3 @@ class User(db.Model):
     password = db.Column(db.String(80))
     email = db.Column(db.String(100), unique=True)
     phone = db.Column(db.String(15))
-    subscription = db.Column(db.String(50), default='gratis')
